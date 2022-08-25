be7c7a71eb23c1642be3934850ad375174302659
hernancandelino <hernancandelino@gmail.com>
2022-08-25 19:31:55 -0300

    hago funcionar contador

--- src/components/itemCount/ItemCount.js
+++ src/components/itemCount/ItemCount.js
@@ -1,20 +1,25 @@
 import React, {useState} from 'react';
+import './itemCount.css'
 
 const ItemCount = () => {
     const [cantidad, setCantidad] = useState(0);
 
     const aumentarCantidad = () => {
-        setCantidad(cantidad + 1);
+        if (cantidad >= 0) {
+            setCantidad(cantidad + 1);
+        }
     }
     const disminuirCantidad = () => {
-        setCantidad(cantidad - 1);
+        if (cantidad > 0) {
+            setCantidad(cantidad - 1);
+        }
     }
-
+    
     return (
     <div className="contador">
-        <button className='boton-sumar' onClick={aumentarCantidad()}>+</button>
+        <button className='boton-sumar' onClick={aumentarCantidad}>+</button>
         <p>{cantidad}</p>
-        <button className='boton-restar' onClick={disminuirCantidad()}>-</button>
+        <button className='boton-restar' onClick={disminuirCantidad}>-</button>
     </div>
     )
 }
